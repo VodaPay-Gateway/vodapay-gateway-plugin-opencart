d0223bbbf4948cec89c16c3727f2b6363a3b9f47
vniej002 <juanrouxtersius.vanniekerk@vcontractor.co.za>
2023-02-22 12:09:20 +0200

    Adding better bad response handling.
    Better handling for bad payment response codes. To show the user the error.

--- upload/catalog/language/en-gb/extension/payment/vpg.php
+++ upload/catalog/language/en-gb/extension/payment/vpg.php
@@ -8,34 +8,41 @@ $_['environment_prod']	= 2;
 $_['text_title']	= 'Vodapay Gateway';
 $_['text_testmode']	= 'Warning: The payment gateway is in \'Sandbox Mode\'. Your account will not be charged.';
 $_['text_total']	= 'Shipping, Handling, Discounts & Taxes';
+$_['text_unable']   = 'Unable to locate or update your order status';
+$_['text_declined'] = 'Payment was declined';
+$_['text_failed']   = 'Paymate Transaction Failed';
+$_['text_failed_message']   = '<p>Unfortunately there was an error processing your Paymate transaction.</p><p><b>Warning: </b></p>';
+$_['text_basket']   = 'Basket';
+$_['text_checkout'] = 'Checkout';
+$_['text_success']  = 'Success';
 
 //Button
 $_['button_confirm']	= 'Continue';
 
 //Repsonses
 /* Begin - Good response codes */
-$_['00'] = 'Approved or completed successfully';//RESPONSE_CODE_OK
-$_['08'] = 'Honor with identification';//RESPONSE_CODE_HONOR_WITH_IDENTIFIACTION
-$_['10'] = 'Approved, partial';//RESPONSE_CODE_APPROVED_PARTIAL
-$_['11'] = 'Approved, VIP';//RESPONSE_CODE_APPROVED_VIP
-$_['16'] = 'Approved, update track 3';//RESPONSE_CODE_APPROVED_UPDATE_TRK3
+$_['r00'] = 'Approved or completed successfully';//RESPONSE_CODE_OK
+$_['r08'] = 'Honor with identification';//RESPONSE_CODE_HONOR_WITH_IDENTIFIACTION
+$_['r10'] = 'Approved, partial';//RESPONSE_CODE_APPROVED_PARTIAL
+$_['r11'] = 'Approved, VIP';//RESPONSE_CODE_APPROVED_VIP
+$_['r16'] = 'Approved, update track 3';//RESPONSE_CODE_APPROVED_UPDATE_TRK3
 $_['good_response'] = '00'.','.'08'.','.'10'.','.'11'.','.'16';
 /* End - Good response codes */
 
 /* Begin - Bad response codes */
-$_['03'] = 'Invalid merchant';//RESPONSE_CODE_INVALID_MERCHANT
-$_['04'] = 'Pick-up card';//RESPONSE_CODE_PICKUP_CARD
-$_['05'] = 'Do not honor';//RESPONSE_CODE_DO_NOT_HONOR
-$_['06'] = 'Error';//RESPONSE_CODE_ERROR
-$_['17'] = 'Customer cancellation';//RESPONSE_CODE_CUSTOMER_CANCELLATION
-$_['26'] = 'Duplicate record';//RESPONSE_CODE_DUPLICATE_RECORD
-$_['51'] = 'Insufficient funds';//RESPONSE_CODE_INSUFFICIENT_FUNDS
-$_['54'] = 'Expired card';//RESPONSE_CODE_EXPIRED_CARD
-$_['56'] = 'No card record';//RESPONSE_CODE_NO_CARD_RECORD
-$_['68'] = 'Response received too late';//RESPONSE_CODE_RESPONSE_RECEIVED_TOO_LATE
-$_['91'] = 'Issuer or switch inoperative';//RESPONSE_CODE_ISSUER_OR_SWITCH_INOPERATIVE
-$_['92'] = 'Routing error';//RESPONSE_CODE_ROUTING_ERROR
-$_['96'] = 'System malfunction';//RESPONSE_CODE_SYSTEM_MALFUNCTION
+$_['r03'] = 'Invalid merchant';//RESPONSE_CODE_INVALID_MERCHANT
+$_['r04'] = 'Pick-up card';//RESPONSE_CODE_PICKUP_CARD
+$_['r05'] = 'Do not honor';//RESPONSE_CODE_DO_NOT_HONOR
+$_['r06'] = 'Error';//RESPONSE_CODE_ERROR
+$_['r17'] = 'Customer cancellation';//RESPONSE_CODE_CUSTOMER_CANCELLATION
+$_['r26'] = 'Duplicate record';//RESPONSE_CODE_DUPLICATE_RECORD
+$_['r51'] = 'Insufficient funds';//RESPONSE_CODE_INSUFFICIENT_FUNDS
+$_['r54'] = 'Expired card';//RESPONSE_CODE_EXPIRED_CARD
+$_['r56'] = 'No card record';//RESPONSE_CODE_NO_CARD_RECORD
+$_['r68'] = 'Response received too late';//RESPONSE_CODE_RESPONSE_RECEIVED_TOO_LATE
+$_['r91'] = 'Issuer or switch inoperative';//RESPONSE_CODE_ISSUER_OR_SWITCH_INOPERATIVE
+$_['r92'] = 'Routing error';//RESPONSE_CODE_ROUTING_ERROR
+$_['r96'] = 'System malfunction';//RESPONSE_CODE_SYSTEM_MALFUNCTION
 $_['D1'] = 'Invalid Service Level or level exceeded.';//RESPONSE_CODE_INVALID_SERVICE_LEVEL
 $_['D2'] = 'Invalid transaction parameteres usage';//RESPONSE_CODE_INVALID_TRANSACTION_PARAMETERS_USAGE
 $_['D3'] = 'Repeat attempted, but certain parameters do not match.';//RESPONSE_CODE_REPEAT_PARAMETER_MISMATCH
